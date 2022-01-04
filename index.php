789e26085678b0607fed3f600477b749a58cda43
[Lukmon Rasaq] <[rasaqlukmon28@gmail.com]>
2022-01-04 10:48:40 -0500

    changes made

--- index.php
+++ index.php
@@ -204,14 +204,14 @@
                     <i class="bi bi-laptop"></i>
                   </div>
                   <h3 class="card-title mb-3" style="font-family:'Times New Roman', Times, serif"><span class="text-success">Mission of MAGIC</span></h3>
-                  <p class="card-text collapse" id= "collapseContent" style="font-family:'Times New Roman', Times, serif">
+                  <p class="card-text" style="font-family:'Times New Roman', Times, serif">
                     The primary mission of the council is to forge connections and build relationships with local, regional, and national agencies and leaders that will help advance research and academic programming <!--for the university. We are particularly interested in aligning this research work with areas of strategic importance for the university, including artificial intelligence and intelligent systems, cybersecurity, marine science and engineering, and remoote sensing, among others.-->
                   </p>
                   <!-- <a class="btn btn-primary" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
                   Read More
                   </a> -->
                   <!-- <b type="button" class="btn btn-secondary" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-content="Bottom popover">Read More</b> -->
-                  <a href="#collapseContent" class="btn btn-primary collapsed" style="font-family:'Times New Roman', Times, serif" data-toggle="collapse"  aria-expanded="false" aria-controls="collapseContent">Read More</a>
+                  <a href="#collapseContent" class="btn btn-primary" style="font-family:'Times New Roman', Times, serif">Read More</a>
               <!-- <div class="collapse" id="collapseExample">
                   <div class="card card-body">
                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
